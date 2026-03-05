14920ea7819e04db4dec5560de82e0a8343ffba4
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-05 18:46:14 +0530

    actions

--- vpc.tf
+++ vpc.tf
@@ -2,5 +2,5 @@ module "vpc" {
     source              = "../terraform-aws-vpc" # In projects, we use "git::https://github.com/javeed-mohd/terraform-aws-vpc.git?ref=main", to directly get it from git if someone updated...
     project             = var.project_name # project/project_name can be used as you wish(Not necessarily it should be same but it gives the exact default value provided).
     environment         = var.env
-    # is_peering_required = true 
+    is_peering_required = true 
 }
