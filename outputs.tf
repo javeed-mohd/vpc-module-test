dd605b7c37b1023c606e5c92024c8ac8bc83e761
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-05 16:21:56 +0530

    actions

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-# To comment after route_table creation
+# when outputs are unnecessary for testing or when they reference values that are not available, to prevent Terraform errors and keep the test environment simple. We comment this...
 /* output "azs_info" {
     value = module.vpc.azs_info
 } */
